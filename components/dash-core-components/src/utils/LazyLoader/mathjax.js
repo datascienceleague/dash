773f25c4749a5076d44747b2617d5fa16a434c77
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2022-02-21 16:37:49 -0500

    mathjax es6 > es5

--- components/dash-core-components/src/utils/LazyLoader/mathjax.js
+++ components/dash-core-components/src/utils/LazyLoader/mathjax.js
@@ -1,5 +1,5 @@
 export default () => Promise.resolve(window.MathJax ||
-    import(/* webpackChunkName: "mathjax" */ 'mathjax-full/components/src/tex-svg/tex-svg.js').then(() => {
+    import(/* webpackChunkName: "mathjax" */ 'mathjax/es5/tex-svg.js').then(() => {
         window.MathJax.config.startup.typeset = false;
 
         return window.MathJax;
